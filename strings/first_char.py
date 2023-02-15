131845db7257d468605753676f6d0256d753e11f
Stanislav <stanislav@MacBookU.local>
2023-02-15 03:12:27 +0100

    create folders

---
+++ strings/first_char.py
@@ -0,0 +1,37 @@
+'''
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+Input: s = "aabb"
+Output: -1
+ 
+
+Constraints:
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+
+Results:
+    Runtime: 83 ms
+    Memory Usage: 14 MB
+    Beats 90% of python3 submission
+'''
+
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        char_dict = Counter(s)
+        c = 0
+        for i in s:
+            if char_dict[i] == 1:
+                return c
+            else:
+                c += 1
+        return -1
