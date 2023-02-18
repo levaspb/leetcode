af7e30091516da3c7a948ff4a5f0ed691adcdd0a
Stanislav <stanislav@MacBookU.local>
2023-02-18 17:38:10 +0100

    better solution for reverse_int

--- strings/reverse_int.py
+++ strings/reverse_int.py
@@ -7,11 +7,11 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
 class Solution:
     def reverse(self, x: int) -> int:
         s = str(abs(x))
-        if x < 0: 
-            if -int(s[::-1]) < -2**31: return 0
-            return -int(s[::-1]) 
+        if x < 0:
+            result = -int(s[::-1])
+            if result < -2147483648: return 0
         else:
-            if int(s[::-1]) > 2**31-1: return 0
-            return int(s[::-1])
-
+            result = int(s[::-1])
+            if result > 2147483647: return 0
+        return result
 
